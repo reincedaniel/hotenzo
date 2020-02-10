2acdd242e3885bd0870bb034bc5aaebe7033648d
Lourenço Daniel Sebastião Carlos <reincedaniel@gmail.com>
2020-02-10 17:06:34 +0100

    Signed-off-by: Lourenço Daniel Sebastião Carlos <reincedaniel@gmail.com>

---
+++ models/category.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (sequelize, DataTypes) {
+  var Category = sequelize.define('Category', {
+    name: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    status: DataTypes.BOOLEAN
+  }, {});
+
+  Category.associate = function (models) {
+    Category.hasMany(models.Article);
+  };
+
+  return Category;
+};
